611d0d78121dc21a604f60072e01797b0bb675c7
T-102 <T-102@PC248>
2018-01-29 19:15:05 -0600

    Constructores

---
+++ poo-Asencio/src/constructores/Probarconstructores.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package constructores;
+import javax.swing.*;
+
+/**
+ *
+ * @author T-102
+ */
+public class Probarconstructores {
+    public static void main(String[] args) {
+        //Alumno a=new Alumno();
+        //System.out.println("Tu edad es "+a.getNombre());
+        int x=02;
+        System.out.println(x);
+        JFrame v=new JFrame("Adios");
+        v.setSize(400,200);
+        v.setVisible(true);
+        
+    }
+}
