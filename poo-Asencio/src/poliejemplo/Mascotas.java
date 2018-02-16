b9c149bd04fcb785d2d2e6640002e10f74bbf0ef
T-102 <T-102@PC248>
2018-02-15 19:43:45 -0600

    ejercicio resuelo y arraylist

--- poo-Asencio/src/poliejemplo/Mascotas.java
+++ poo-Asencio/src/poliejemplo/Mascotas.java
@@ -4,13 +4,14 @@
  * and open the template in the editor.
  */
 package poliejemplo;
-
+import java.util.*;
 /**
  *
  * @author T-102
  */
 public class Mascotas {
     public static void main(String[] args) {
+       
         Animal animales[]=new Animal[12];
         
         //primero generamos alos gatos
@@ -36,11 +37,24 @@ public class Mascotas {
          ServicioAnimales s=new ServicioAnimales();
         
         //la magia del poliformismo!!
+        //Expresion Expresiva
         for(Animal a:animales)
         {
         s.servicioHacerRuido((ComportamientoAnimal)a);
         }
-        
+        //Vams a generar un array list y poner ahi animales
+        ArrayList<Animal>animalitos=new ArrayList<>();
+        animalitos.add(new Pollo());
+        animalitos.add(new Raton ());
+        animalitos.add(new Perro ());
+        animalitos.add(new Gato());
+        animalitos.add(new Perro());
+        for(Animal a:animalitos)
+        {
+            s.servicioHacerRuido((ComportamientoAnimal)a);
+        }
+        animalitos.get(0).setEdad(1);
+        System.out.println(animalitos.get(0).getEdad());
     }
     
 }
